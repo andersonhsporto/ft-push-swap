255c175b01318532558806edc87799173dfbc1e1
anhigo-s <anderson.higo2@gmail.com>
2021-12-18 03:04:36 -0300

    update

--- source/sort/algorithm_medium.c
+++ source/sort/algorithm_medium.c
@@ -6,7 +6,7 @@
 /*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 01:37:12 by anhigo-s          #+#    #+#             */
-/*   Updated: 2021/12/18 00:52:36 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2021/12/18 02:00:11 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,19 @@ void	quick_test(t_swap *data)
 
 	stk_a = array_to_list(data);
 	stk_b = NULL;
-	while (i < (data->store.len_stack_a / 2) - 1)
+	while (i < (data->store.len_stack_a / 2) - 2)
 	{
 		//printf("%d %d \n", data->merge.grp_min[i], data->merge.grp_max[i]);
 		move_fourths(&stk_a, &stk_b, data->merge.grp_min[i], data->merge.grp_max[i]);
 		i++;
 	}
 	sort_stack(&stk_a, 0);
-	printlist(stk_a);
-	// reverse_sort_stack(&stk_b, 1);
-	// while (stk_b != NULL)
-	// {
-	// 	list_push(&stk_b, &stk_a, pb);
-	// }
+	//printlist(stk_a);
+	//reverse_sort_stack(&stk_b, 1);
+	while (stk_b != NULL)
+	{
+		list_push(&stk_b, &stk_a, pa);
+	}
 	// printlist(stk_a);
 }
 
